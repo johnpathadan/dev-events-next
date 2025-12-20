5425beaa4c63349b29e903d52bd302b898eeacd3
John Pathadan <john.paul@vonnue.com>
2025-12-20 13:55:42 +0530

    api routes partial completion

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com", //this is where
+        //cloudinary images are going to be stored
+      },
+    ],
+  },
 };
 
 export default nextConfig;
